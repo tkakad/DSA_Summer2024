89d5c834207e31ecf153932155ce1a005d351f5c
Tejas Kakad <tejaskakad@connect-172-31-4-160.vpn.tamu.edu>
2024-06-07 15:36:48 +0300

    Stack using Arrays done

---
+++ Stack/StackUsingArray.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+// Stack structure declaration
+
+struct Stack{
+    int size, top, *s;
+};
+
+// Stack ADT Operations
+
+void push(Stack *st, int x){
+    if (st->top == st->size - 1)
+        cout << "Stack Overflow!" << endl;
+
+    else{
+        st->top++;
+        st->s[st->top] = x;
+    }
+}
+
+int pop(Stack *st){
+    int x = -1;
+
+    if (st->top == -1)
+        cout << "Stack Underflow!" << endl;
+
+    else{
+        x = st->s[st->top];
+        st->top--;
+    }
+
+    return x;
+}
+
+int peek(Stack *st, int pos){
+    int x = -1;
+    
+    if (st->top - pos + 1)
+        cout << "Invalid Position!" << endl;
+
+    else
+        x = st->s[st->top - pos + 1];
+
+    return x;
+}
+
+int stackTop(Stack st){
+    if (st.top == -1)
+        return -1;
+    
+    else
+        return st.s[st.top];
+}
+
+int isEmpty(Stack st){
+    if (st.top == -1)
+        return 1;
+    
+    else
+        return 0;
+}
+
+int isFull(Stack st){
+    if(st.top == st.size - 1)
+        return 1;
+    
+    else
+        return 0;
+}
+
+void Display(Stack st){
+    for (int i = st.top; i >= 0; i++)
+        cout << st.s[i] << " ";
+    
+    cout << endl;
+}
+
+// Main funciton
+
+int main(){
+    struct Stack st;
+    int size;
+
+    cout << "Enter the stack size: ";
+    cin >> st.size;
+
+    st.s = new int[st.size];
+    st.top = -1;
+
+    // Performing the above declared operations on Stack
+
+    push(&st, 10);
+    push(&st, 20);
+    push(&st, 30);
+    push(&st, 40);
+
+    Display(st);
+
+    return 0;
+} 
